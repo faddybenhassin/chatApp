7ce449fc8749381463964c177ec7525927f1024b
faddy <faddybenhassin@gmail.com>
2026-01-07 22:30:50 +0100

    logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -41,7 +41,7 @@ export async function signup(req, res) {
             _id: savedUser._id,
             email: savedUser.email,
             username: savedUser.username,
-            profilePic: savedUser.profilePic,
+            profilePic: savedUser.profilePicUrl,
         });
     } catch (err) {
         console.error("error in signup controller", err);
@@ -88,7 +88,7 @@ export async function login(req,res){
             _id: user._id,
             email: user.email,
             username: user.username,
-            profilePic: user.profilePic,
+            profilePic: user.profilePicUrl,
         });
     } catch (err) {
         console.log("error in login controller: ", err.message);
@@ -96,6 +96,18 @@ export async function login(req,res){
     }
 };
 
-export async function logout(req,res){
-    res.send("logout");
+export async function logout(req, res) {
+    // Clear auth cookie on logout
+    try {
+        res.clearCookie("jwt", {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV !== "dev",
+        });
+        return res.status(200).json({ message: "Logged out" });
+    } catch (err) {
+        console.log("error in login controller: ", err.message);
+        return res.status(500).json({ message: "Internal server error" });
+    }
 };
